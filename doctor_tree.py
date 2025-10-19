d6e85ebae3de899367746abefca62e0fc5e4a8a0
Alex Sanchez <155488403+aleexsaanchez@users.noreply.github.com>
2025-10-19 17:33:42 -0400

    Programs finished and Memo uploaded

--- doctor_tree.py
+++ doctor_tree.py
@@ -1,12 +1,65 @@
 class DoctorNode:
-    pass # Delete this line and implement the DoctorNode class
-
-
+    def __init__(self,name):
+        self.name = name #Dr's Name
+        self.left = None 
+        self.right = None
 
 class DoctorTree:
-    pass # Delete this line and implement the DoctorTree class
+    def __init__(self):
+        self.root = None #Root level doctor
+    
+    def insert(self, parentName, childName, side): #adds a new doctor under root doctor
+        def insertRecursive(node): #function created to search the tree recursively to find a parent
+            if node is None:
+                return False
+            if node.name == parentName:
+                if side == "left" and node.left is None:
+                    node.left = DoctorNode(childName)
+                    return True
+                if side == "right" and node.right is None:
+                    node.right = DoctorNode(childName)
+                    return True
+                else:
+                    print(f"Error: {side} side is already occupied for Doctor {parentName}.")
+                    return False
+                
+            return insertRecursive(node.left) or insertRecursive(node.right)
+        if not insertRecursive(self.root):
+            print(f"Error: Doctor {parentName} was not found. ")
+
+    #Traversal Methods
+    #Preorder
+    def preorder(self, node):
+        if node is None:
+            return []
+        return [node.name] + self.preorder(node.left) + self.preorder(node.right)
+    #This function visits the root and then left subtree then right subtree
+
+    #Inorder
+    def inorder(self, node):
+        if node is None:
+            return []
+        return self.inorder(node.left) + [node.name] + self.inorder(node.right)
+    #This function visit the left subtree first, then root, then right subtree
+     
+    #Postorder 
+    def postorder(self, node):
+        if node is None:
+            return []
+        return self.postorder(node.left) + self.postorder(node.right) + [node.name]
+    #This function first visit the left subtree, then right subtree, then the root
 
 
 
 
 # Test your DoctorTree and DoctorNode classes here
+tree = DoctorTree()
+tree.root = DoctorNode("Dr. Gutierrez")
+tree.insert("Dr. Gutierrez", "Dr. Sanchez", "left")
+tree.insert("Dr. Gutierrez", "Dr. Andrews", "right")
+tree.insert("Dr. Andrews", "Dr. St. CLair", "left")
+tree.insert("Dr. Andrews", "Dr. Ribeiro", "right")
+
+print("Preorder: ", tree.preorder(tree.root))
+print("Inorder: ", tree.inorder(tree.root))
+print("Postorder: ", tree.postorder(tree.root))
--- emergency_queue.py
+++ emergency_queue.py
@@ -1,12 +1,81 @@
 class Patient:
-    pass # Delete this line and implement the Patient class
+    def __init__(self, name, urgency):
+        self.name = name #patient
+        self.urgency = urgency #level of urgency
 
 
 
 class MinHeap:
-    pass # Delete this line and implement the MinHeap class
+    def __init__(self):
+        self.data = [] #list of patient objects 
+    
+    def heapifyUp(self, index): #index is the urgency level, 0 being the most urgent
+        while index > 0:
+            parent = (index - 1) // 2
+            if self.data[index].urgency < self.data[parent].urgency: #comparing old patient with new patient, if new patient is more urgent then swaps them
+                self.data[index], self.data[parent] = self.data[parent], self.data[index]
+                index = parent
+            else:
+                break
+
+    def heapifyDown(self, index): #function will be used after a patient is removed or being taken care of
+        size = len(self.data)
+        while index < size:
+            left = 2 * index + 1
+            right = 2 * index + 2
+            smallest = index
+            #comparing patients and updating the order
+            if left < size and self.data[left].urgency < self.data[smallest].urgency:
+                smallest = left
+            if right < size and self.data[right].urgency < self.data[smallest].urgency:
+                smallest = right
+            
+            if smallest != index:
+                self.data[index], self.data[smallest] = self.data[smallest], self.data[index]
+                index = smallest
+            else:
+                break
+
+    #core heap methods
+    # 
+    def insert(self, patient): #adds a new patient and calls heapify to restore order if needed
+        self.data.append(patient)
+        self.heapifyUp(len(self.data) - 1)    
+
+    def printHeap(self):
+        print("Current queue: ") 
+        for patient in self.data:
+            print(f"-Patient {patient.name} urgency ({patient.urgency})")
+
+    def peek(self): #returns the most urgent patients
+        return self.data[0] if self.data else None
+    
+    def removeMin(self): #removes the most urgent patient from the list, used when that patient has been taken care of
+        if not self.data:
+            return None
+        minPatient = self.data[0]
+        lastPatient = self.data.pop()
+        if self.data:
+            self.data[0] = lastPatient
+            self.heapifyDown(0)
+        return minPatient
+
+
 
 
 
 
 # Test your MinHeap class here including edge cases
+
+heap = MinHeap()
+heap.insert(Patient("Alex", 3))
+heap.insert(Patient("Ella", 1))
+heap.insert(Patient("Guti", 5))
+heap.printHeap()
+
+next_up = heap.peek()
+print("Next up:", next_up.name, next_up.urgency)
+
+served = heap.removeMin()
+print("Helped:", served.name)
+heap.printHeap()
